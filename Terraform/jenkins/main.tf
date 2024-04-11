ccd4fc16ec0ac5a1b9d2f30a5bb84dd7abf5b4ba
Mamaliprusty <109724660+Mamaliprusty@users.noreply.github.com>
2024-04-11 19:27:14 +0530

    Update main.tf

--- Terraform/jenkins/main.tf
+++ Terraform/jenkins/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Jenkinsserver" {
-  ami                    = data.aws_ami.amazonubuntu.id
+  ami                    = data.aws_ami.amazonlinux2.id
   instance_type          = var.my_instance_type
   key_name               = var.my_key
   vpc_security_group_ids = [aws_security_group.web-traffic.id]
