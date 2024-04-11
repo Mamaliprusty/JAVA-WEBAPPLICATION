f360e47454d32567c533d347d1068aea6ada227c
Mamaliprusty <109724660+Mamaliprusty@users.noreply.github.com>
2024-04-11 15:23:07 +0530

    Create data.tf

---
+++ Terraform/jenkins/data.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "amazonlinux2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ami-05fb0b8c1424f266b"]
+  }
+}
